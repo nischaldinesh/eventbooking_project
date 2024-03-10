bb6c518fa14d24be8f304a3cacf2a920cea89920
Nischal Dinesh <nischaldgowda77@gmail.com>
2024-03-10 15:51:09 +0530

    Implemented all basic CRUD operations

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,21 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
+import Collection from "@/components/shared/Collection";
+import { getAllEvents } from "@/lib/actions/events.actions";
+import { SearchParamProps } from "@/types";
 
-export default function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || "";
+  const category = (searchParams?.category as string) || "";
+  const events = await getAllEvents({
+    query: searchText,
+    category,
+    page,
+    limit: 6,
+  });
+
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
@@ -39,6 +52,16 @@ export default function Home() {
         <div className="flex w-full flex-col gap-5 md:flex-row">
           Search CategoryFilter
         </div>
+
+        <Collection
+          data={events?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={1}
+          totalPages={2}
+        />
       </section>
     </>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
+
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['utfs.io']
+    },
+    remotePatters: [
+        {
+            protocol: 'https',
+            hostname: 'utfs.io',
+            port: ''
+        }
+    ]
+};
 
 export default nextConfig;
